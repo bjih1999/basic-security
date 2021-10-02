7481394a143892365173bceec9372eb84ba8c2c2
Jihyun <bjih1999@naver.com>
2021-10-02 17:40:40 +0900

    feat: 권한 설정

--- src/main/java/io/security/SecurityConfig.java
+++ src/main/java/io/security/SecurityConfig.java
@@ -2,6 +2,7 @@ package io.security;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -28,9 +29,32 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     private final UserDetailsService userDetailsService;
 
     @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                    .inMemoryAuthentication()
+                    .withUser("user")
+                    .password("{noop}1111")
+                    .roles("USER")
+                .and()
+                    .withUser("sys")
+                    .password("{noop}1111")
+                    .roles("SYS", "USER")
+                .and()
+                    .withUser("admin")
+                    .password("{noop}1111")
+                    .roles("ADMIN", "SYS", "USER")
+
+
+        ;
+    }
+
+    @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                     .authorizeRequests()
+                    .antMatchers("/user").hasRole("USER")
+                    .antMatchers("/admin/pay").hasRole("ADMIN")
+                    .antMatchers("/admin/**").access("hasRole('ADMIN') or hasRole('SYS')")
                     .anyRequest()
                     .authenticated()
                 .and()
