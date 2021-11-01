12ad62786d053f6ce9c63a7cb2c69128f8909d0c
Jihyun <bjih1999@naver.com>
2021-11-01 21:53:55 +0900

    feat: 권한정보-DB 매핑 클래스 구현

---
+++ src/main/java/io/security/security/factory/UrlResourceMapFactoryBean.java
@@ -0,0 +1,42 @@
+package io.security.security.factory;
+
+import org.springframework.beans.factory.FactoryBean;
+import org.springframework.security.access.ConfigAttribute;
+import org.springframework.security.web.util.matcher.RequestMatcher;
+
+import java.util.LinkedHashMap;
+import java.util.List;
+
+public class UrlResourceMapFactoryBean implements FactoryBean<LinkedHashMap<RequestMatcher, List<ConfigAttribute>>> {
+
+    private SecurityResourceSevice securityResourceSevice;
+
+    private LinkedHashMap<RequestMatcher, List<ConfigAttribute>> resourceMap;
+
+
+    /*
+    싱글톤 처리를 할것인지 설정하는 함수
+    */
+    @Override
+    public boolean isSingleton() {
+        return true;
+    }
+
+    @Override
+    public LinkedHashMap<RequestMatcher, List<ConfigAttribute>> getObject() throws Exception {
+        if (resourceMap == null) {
+            init();
+        }
+
+        return resourceMap;
+    }
+
+    private void init() {
+        resourceMap = securityResourceSevice.getResourceList();
+    }
+
+    @Override
+    public Class<?> getObjectType() {
+        return LinkedHashMap.class;
+    }
+}
